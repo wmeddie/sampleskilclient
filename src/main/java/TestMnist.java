3da2e5c2e5a2112b6b4a81beca5d62ff235caed9
Eduardo Gonzalez <me@wm-eddie.com>
2018-07-09 07:03:25 +0900

    Test mnist with real data.

--- src/main/java/TestMnist.java
+++ src/main/java/TestMnist.java
@@ -4,10 +4,13 @@ import io.skymind.skil.predict.client.PredictServiceClient;
 import io.skymind.skil.service.model.ClassificationResult;
 import io.skymind.skil.service.model.MultiClassClassificationResult;
 import io.skymind.skil.service.model.Prediction;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.factory.Nd4j;
 
 import java.util.Arrays;
+import java.util.UUID;
 
 public class TestMnist {
     public static void main(String... args) throws Exception {
@@ -17,8 +20,12 @@ public class TestMnist {
 
         //ModelHistoryClient mhClient = new ModelHistoryClient("http://127.0.0.1:9100");
 
-        //String basePath = "http://127.0.0.1:9008/endpoints/demo/model/mnist/default";
         String basePath = "http://localhost:9008/endpoints/demo/model/sample-mnist-model/default";
+        //String basePath = "http://127.0.0.1:9008/endpoints/demo/model/mnist/default";
+        //String basePath = "http://localhost:9008/endpoints/demo/model/sample-mnist-model/default";
+        //String basePath = "http://localhost:9008/endpoints/demo/model/tfmodel/default";
+        //String basePath = "http://localhost:9008/endpoints/demo/model/keras-mnist-model/default";
+
         //String basePath = "http://localhost:9601";
         PredictServiceClient client = new PredictServiceClient(basePath);
         client.setAuthToken(authToken);
@@ -31,6 +38,22 @@ public class TestMnist {
 
         System.out.println(result);
 
+        MnistDataSetIterator data = new MnistDataSetIterator(1, 100);
 
+        while (data.hasNext()) {
+            DataSet ds = data.next();
+            INDArray req = ds.getFeatures();
+            INDArray label = ds.getLabels();
+
+            ClassificationResult classify = client.classify(new Prediction(req, UUID.randomUUID().toString()));
+            int truth = Nd4j.argMax(label, -1).max(-1).data().getInt(0);
+            int pred = classify.getResults()[0];
+
+            System.out.println(
+                    "True: " + truth +
+                    " Predicted: " + pred +
+                    " Match: " + ((truth == pred) ? "true" : "false")
+            );
+        }
     }
 }
